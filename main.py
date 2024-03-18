a9cf142f23a18015db60eade4fe7e0b0a9b79503
Debbie <tkudebby@gmail.com>
2024-03-18 23:02:57 +0800

    完成BMI計算初步功能&置入圖表

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
 
 app = Flask(__name__)
 
 
-@app.route("/health")
+@app.route("/health", methods=["GET", "POST"])
 def health():
-    return render_template("health.html")
+    bmi = None
+    if request.method == "POST":
+        try:
+            height = eval(request.form.get("height"))
+            weight = eval(request.form.get("weight"))
+            bmi = get_bmi(height, weight)
+        except:
+            bmi = "輸入有誤"
+    return render_template("health.html", bmi=bmi)
+
+
+def get_bmi(height, weight):
+    return round(weight / (height / 100) ** 2, 2)
 
 
 @app.route("/")
